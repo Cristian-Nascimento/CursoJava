dd63625072d0877e0aedf45501ff9cc6ae19eabe
@Cristian-Nascimento <cristianrosanascimento@gmail.com>
2022-05-16 12:27:02 -0300

    Commit exercício de conversor em Java

---
+++ Conversor Temperatura/src/converterTemperatura/temperatura.java
@@ -0,0 +1,18 @@
+package converterTemperatura;
+
+public class temperatura {
+
+	public static void main(String[] args) {
+		// Fórmula conversor temperatura: (°F - 32) x 5/9 = °C
+		final double FATOR = 5.0/9.0;
+		final double AJUSTE = 32;
+		double fahrenheit = 86;
+		double celsius = ( fahrenheit - AJUSTE ) * FATOR;
+		System.out.println("A conversão de 86°F para °C é:" + celsius + "°C");
+
+		fahrenheit = 50;
+		celsius = ( fahrenheit - AJUSTE ) * FATOR;
+	System.out.println("A conversão de 86°F para °C é:" + celsius + "°C");
+
+}
+}
